83241273834a630ed200d801205c74aec8315ca9
shadan malik <shadanmalik@1981>
2026-06-19 16:41:31 +0530

    hey

--- hi.py
+++ hi.py
@@ -5,7 +5,7 @@ a = int(input("enter the day:"))
 b = int(input("enter the month:"))
 c = int(input("enter the year:"))
 
-print ("your date of birth is on ok brother:", a,b,c)
+print ("of birth is on ok brother:", a,b,c)
 
 
  
