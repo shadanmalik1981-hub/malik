7f40a87130e9e4cae5b12d012b2fe7531449fd40
shadan malik <shadanmalik@1981>
2026-07-02 16:45:00 +0530

    restored deleted files

---
+++ hi.py
@@ -0,0 +1,11 @@
+import random as r
+import calendar as cl
+
+a = int(input("enter the day:"))
+b = int(input("enter the month:"))
+c = int(input("enter the year:"))
+
+print ("birthday of birth is on :", a,b,c)
+
+
+ 
---
+++ repo.py
@@ -0,0 +1,5 @@
+a =int(input(" enter the number"))
+b=int(input(" enter the number"))
+print (a+b)
+
+hello my friend 
